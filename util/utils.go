c9ed554440ea112a46f27bc2d2f5f277b5532a98
Mehdi Azizi <azizimaadii@gmail.com>
2025-11-01 19:46:08 +0330

    To pointer or nil

--- util/utils.go
+++ util/utils.go
@@ -17,3 +17,13 @@ func GetPtrValue[T any, P Pointer[T]](p P) T {
 func ToPtr[T any](v T) *T {
 	return &v
 }
+
+func ToPtrOrNil[T comparable](v T) *T {
+	var zero T
+
+	if v == zero {
+		return nil
+	}
+
+	return &v
+}
